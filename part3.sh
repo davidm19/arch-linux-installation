0eb6544007f35211d092ee40b6b7ede09920053c
davidm19 <dmalone@chadwickschool.org>
2019-06-13 11:13:23 -0700

    Add NetworkManager for easy internet access and remove unnecessary last installation part

--- part3.sh
+++ part3.sh
@@ -11,8 +11,23 @@ echo "What hostname do you want your computer to have? "
 read -r hostname
 echo "$hostname" >/etc/hostname
 
-echo "Please set up your ethernet/internet according to this manner: "
-echo -e "\t systemctl enable dhcpcd@<ethernet_device_id>.service"
-echo "OR"
-echo -e "\t systemctl enable netctl@<wifi_card_id>.service"
-echo -e "\n NOTE: netctl may need to be installed in order for this to work. If not, please install it now."
+# Install and enable network-manager and other tools
+pacman -S networkmanager network-manager-applet wpa_supplicant
+systemctl enable NetworkManager.service
+
+# Set up root passwords and users
+pacman -S sudo bash-completion
+echo "Enter password for ROOT"
+passwd
+echo "What will your username be? "
+read -r username
+useradd -m -g users -G wheel,storage,power -s /bin/bash "$username"
+passwd "$username"
+EDITOR=nano visudo
+
+# Execute final installation commands
+mkinitcpio -p linux
+pacman -S grub  efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+echo "You're done. Now exit from chroot, unmount the device (umount -R /mnt), reboot, and exit the installation media."
